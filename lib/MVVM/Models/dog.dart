46bae64f4a80d9fa94317e4e4ab58a56fd9fe305
Megalomizer <112645496+Megalomizer@users.noreply.github.com>
2024-02-22 21:59:53 +0100

    Upload project to Github

---
+++ lib/MVVM/Models/dog.dart
@@ -0,0 +1,96 @@
+// ignore_for_file: unused_import
+
+import 'dart:async';
+
+import 'package:workouttracker/main.dart';
+
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+/// Define class
+class Dog{
+  final int id;
+  final String name;
+  final int age;
+
+  const Dog({
+    required this.id,
+    required this.name,
+    required this.age,
+  });
+
+  // Convert Dog to a Map -> Keys must correspond to the columns in the database!
+  Map<String, Object?> toMap(){
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+    };
+  }
+
+  /// Override the toString method of the Dog class
+  /// makes it easier to see information about each dog
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, age: $age}';
+  }
+}
+
+/// Insert a dog into the database
+Future<void> insertDog(Dog dog) async {
+  // get a reference to the database
+  Future<Database> database = getDatabase();
+  final db = await database;
+
+  // Insert Dog into the database
+  await db.insert(
+    'dog',
+    dog.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace, // If it exists -> replace
+  );
+}
+
+/// Get a list of all dogs in the database
+Future<List<Dog>> getAllDogs() async {
+  Future<Database> database = getDatabase();
+  final db = await database;
+
+  // query the table for all the dogs
+  final List<Map<String, Object?>> allDogs = await db.query('dogs');
+
+  // convert to a list of objects
+  return [
+    for(final {
+      'id': id as int,
+      'name': name as String,
+      'age': age as int,
+      } in allDogs)
+      Dog(id: id, name: name, age: age),
+  ];
+}
+
+/// Update an entry of Dog in the database
+Future<void> updateDog(Dog dog) async {
+  Future<Database> database = getDatabase();
+  final db = await database;
+
+  await db.update(
+    'dogs',
+    dog.toMap(),
+    where: 'id = ?', // ensure that dog has matching id
+    whereArgs: [dog.id], // pass dog.id as whereArgs to prevent SQL injection
+  );
+}
+
+/// Delete an entry if Dog in the database by ID
+Future<void> deleteDog(int id) async {
+  Future<Database> database = getDatabase();
+  final db = await database;
+
+  await db.delete(
+    'dogs',
+    where: 'id = ?',
+    whereArgs: [id],
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart'; // import flutter package material -> Used to create UI
+import 'package:flutter/widgets.dart'; // Required to avoid errors caused by flutter upgrade
+import 'package:sqflite/sqflite.dart'; // SQlite package
+import 'package:path/path.dart';
+import 'dart:async';
+
+import 'MVVM/Views/firstscreen.dart';
+import 'MVVM/Views/secondscreen.dart';
+import 'MVVM/Views/thirdscreen.dart';
+
+// Entry point into the application -> The very first beginning
+void main() {
+  runApp(const MyApp());
+
+  // Avoid errors caused by flutter upgrade.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Create and open a SQLite database and store the reference
+  final database = getDatabase();
+
+  Future<Database> GetDatabaseInstance() async {
+    return database;
+  }
+} 
+
+// Open the SQLite database and store the reference.
+Future<Database> getDatabase() async {
+  // Set SQLite statements with column names, types and properties
+  String createDogsTable = 'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)';
+
+  // Create and return connection
+  return openDatabase(
+    join(await getDatabasesPath(), 'doggie_database.db'), // Set the path to the database.
+    onCreate: (db, version){
+      // Run the CREATE TABLE statement on the database --> Uses SQLite statements
+      return db.execute(
+        createDogsTable,
+      );
+    },
+    version: 1,
+  );
+}
+
+String title = "WHAAAAAAAAAA";
+// FIXME: relocate widget itself -> only redirect from main actual page needs its own file
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  // this widget is the root of your application
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "TabbedPages!", // Title of the application
+      // Theme of the widget
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // The inner UI of the application -> We set another widget -> A new class
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.directions_car),
+                ),
+                Tab(
+                  icon: Icon(Icons.view_list),
+                ),
+                Tab(
+                  icon: Icon(Icons.email),
+                ),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              FirstScreen(),
+              SecondScreen(),
+              ThirdScreen(),
+            ],
+          ),
+          drawer: Drawer(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: const Text(
+                    "Heading",
+                    style: TextStyle(
+                      fontSize: 42.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    title,
+                  ),
+                  onTap: item1tapped,
+                ),
+                const ListTile(
+                  title: Text(
+                    "Revert"
+                  ),
+                  onTap: item2tapped,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void item1tapped(){
+  title = "Tapped :)";
+}
+
+void item2tapped(){
+  title = "WHAAAAAAAAAA";
+}
